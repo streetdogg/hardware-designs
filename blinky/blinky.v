8c0aaf014a6643142c9382d512858123250b8c65
Piyush Itankar <pitankar@gmail.com>
2021-02-13 08:31:03 +0530

    Organizes the code
    Signed-off-by: Piyush Itankar <pitankar@gmail.com>

--- blinky/blinky.v
+++ blinky/blinky.v
@@ -1,4 +1,4 @@
-/* Copyright 2020 pitankar@gmail.com
+/* Copyright 2021 pitankar@gmail.com
  * 
  * Permission is hereby granted, free of charge, 
  * to any person obtaining a copy of this software
